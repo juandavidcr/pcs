aa85fa9c31a5b51610e94a754119a68985bf376b
juan_david_cr <j1dcrane@gmail.com>
2020-06-25 23:03:20 -0500

    se agrega

---
+++ selected.php
@@ -0,0 +1,8 @@
+ <?php
+	 require_once('settings.config.php');          // Define db configuration arr>
+	 require_once('DBConnection.php');             // Include this file
+
+	 $database = new DBConnection($localhost);      // Create new connection by pa>
+
+	
+ ?>
---
+++ settings.config.php
@@ -0,0 +1,8 @@
+<?php
+	$localhost   = array(
+	'driver' => 'mysql',
+	'host' => '127.0.0.1',
+	'dbname' => 'pcs',
+	'username' => 'root',
+	'password' => 'psytranc3');
+?>
